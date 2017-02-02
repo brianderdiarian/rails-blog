faad1f008c80f92c423bdd4f29e84ae873d31dad
brianderdiarian <brianderdiarian@gmail.com>
2017-02-01 16:09:40 -0800

    Initial commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,48 @@
+class ProjectsController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASSWORD'], except: [:index, :show]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  	@project = Project.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def create
+  	@project = Project.new(project_params)
+
+  	@project.save
+  	redirect_to @project
+  end
+
+  def update
+    @project = Project.find(params[:id])
+ 
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+ 
+    redirect_to projects_path
+  end
+
+  private
+  	def project_params
+  	  params.require(:project).permit(:title, :text)
+	  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+  	@allarticles = Article.all.reverse_order
+  	@articles = Article.all.reverse_order.paginate(:page => params[:page], :per_page => 1)
+  	@allprojects = Project.all
+  end
+end
